de5c61b4e8c22fb01246c4775e9ff7bbc4e28462
henrique <henrique.cunha@ifpb.edu.br>
2021-02-14 18:48:53 -0300

    Adicionando arquivo de teste da biblioteca.

---
+++ grafo_test.py
@@ -0,0 +1,166 @@
+import unittest
+from meu_grafo import *
+from bibgrafo.grafo_exceptions import *
+
+class TestGrafo(unittest.TestCase):
+
+    def setUp(self):
+        # Grafo da Paraíba
+        self.g_p = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p.adicionaAresta('a1', 'J', 'C')
+        self.g_p.adicionaAresta('a2', 'C', 'E')
+        self.g_p.adicionaAresta('a3', 'C', 'E')
+        self.g_p.adicionaAresta('a4', 'P', 'C')
+        self.g_p.adicionaAresta('a5', 'P', 'C')
+        self.g_p.adicionaAresta('a6', 'T', 'C')
+        self.g_p.adicionaAresta('a7', 'M', 'C')
+        self.g_p.adicionaAresta('a8', 'M', 'T')
+        self.g_p.adicionaAresta('a9', 'T', 'Z')
+
+        # Grafo da Paraíba sem arestas paralelas
+        self.g_p_sem_paralelas = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_sem_paralelas.adicionaAresta('a1', 'J', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a2', 'C', 'E')
+        self.g_p_sem_paralelas.adicionaAresta('a3', 'P', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a4', 'T', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a5', 'M', 'C')
+        self.g_p_sem_paralelas.adicionaAresta('a6', 'M', 'T')
+        self.g_p_sem_paralelas.adicionaAresta('a7', 'T', 'Z')
+
+        # Grafos completos
+        self.g_c = MeuGrafo(['J', 'C', 'E', 'P'])
+        self.g_c.adicionaAresta('a1','J','C')
+        self.g_c.adicionaAresta('a2', 'J', 'E')
+        self.g_c.adicionaAresta('a3', 'J', 'P')
+        self.g_c.adicionaAresta('a4', 'E', 'C')
+        self.g_c.adicionaAresta('a5', 'P', 'C')
+        self.g_c.adicionaAresta('a6', 'P', 'E')
+
+        self.g_c2 = MeuGrafo(['Nina', 'Maria'])
+        self.g_c2.adicionaAresta('amiga', 'Nina', 'Maria')
+
+        self.g_c3 = MeuGrafo(['J'])
+
+        # Grafos com laco
+        self.g_l1 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l1.adicionaAresta('a1', 'A', 'A')
+        self.g_l1.adicionaAresta('a2', 'A', 'B')
+        self.g_l1.adicionaAresta('a3', 'A', 'A')
+
+        self.g_l2 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l2.adicionaAresta('a1', 'A', 'B')
+        self.g_l2.adicionaAresta('a2', 'B', 'B')
+        self.g_l2.adicionaAresta('a3', 'B', 'A')
+
+        self.g_l3 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l3.adicionaAresta('a1', 'C', 'A')
+        self.g_l3.adicionaAresta('a2', 'C', 'C')
+        self.g_l3.adicionaAresta('a3', 'D', 'D')
+        self.g_l3.adicionaAresta('a4', 'D', 'D')
+
+        self.g_l4 = MeuGrafo(['D'])
+        self.g_l4.adicionaAresta('a1', 'D', 'D')
+
+        self.g_l5 = MeuGrafo(['C', 'D'])
+        self.g_l5.adicionaAresta('a1', 'D', 'C')
+        self.g_l5.adicionaAresta('a2', 'C', 'C')
+
+        # Grafos desconexos
+        self.g_d = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_d.adicionaAresta('asd', 'A', 'B')
+
+    def test_adiciona_aresta(self):
+        self.assertTrue(self.g_p.adicionaAresta('a10', 'J', 'C'))
+        with self.assertRaises(ArestaInvalidaException):
+            self.assertTrue(self.g_p.adicionaAresta('b1', '', 'C'))
+        with self.assertRaises(ArestaInvalidaException):
+            self.assertTrue(self.g_p.adicionaAresta('b1', 'A', 'C'))
+        with self.assertRaises(ArestaInvalidaException):
+            self.g_p.adicionaAresta('')
+        with self.assertRaises(ArestaInvalidaException):
+            self.g_p.adicionaAresta('aa-bb')
+        with self.assertRaises(ArestaInvalidaException):
+            self.g_p.adicionaAresta('x', 'J', 'V')
+        with self.assertRaises(ArestaInvalidaException):
+            self.g_p.adicionaAresta('a1', 'J', 'C')
+
+    def test_vertices_nao_adjacentes(self):
+        self.assertEqual(self.g_p.vertices_nao_adjacentes(), ['J-E', 'J-P', 'J-M', 'J-T', 'J-Z', 'C-Z', 'E-J', 'E-P', 'E-M', 'E-T', 'E-Z', 'P-J', 'P-E', 'P-M', 'P-T', 'P-Z', 'M-J', 'M-E', 'M-P', 'M-Z', 'T-J', 'T-E', 'T-P', 'Z-J', 'Z-C', 'Z-E', 'Z-P', 'Z-M'])
+
+        self.assertEqual(self.g_p.vertices_nao_adjacentes(),
+                         ['J-E', 'J-P', 'J-M', 'J-T', 'J-Z', 'C-Z', 'E-J', 'E-P', 'E-M', 'E-T',
+                          'E-Z', 'P-J', 'P-E',
+                          'P-M', 'P-T', 'P-Z', 'M-J', 'M-E', 'M-P', 'M-Z', 'T-J', 'T-E', 'T-P',
+                          'Z-J', 'Z-C', 'Z-E',
+                          'Z-P', 'Z-M'])
+
+        self.assertEqual(self.g_c.vertices_nao_adjacentes(), [])
+
+        self.assertEqual(self.g_c3.vertices_nao_adjacentes(), [])
+
+    def test_ha_laco(self):
+        self.assertFalse(self.g_p.ha_laco())
+        self.assertFalse(self.g_p_sem_paralelas.ha_laco())
+        self.assertFalse(self.g_c2.ha_laco())
+        self.assertTrue(self.g_l1.ha_laco())
+        self.assertTrue(self.g_l2.ha_laco())
+        self.assertTrue(self.g_l3.ha_laco())
+        self.assertTrue(self.g_l4.ha_laco())
+        self.assertTrue(self.g_l5.ha_laco())
+
+    def test_grau(self):
+        # Paraíba
+        self.assertEqual(self.g_p.grau('J'), 1)
+        self.assertEqual(self.g_p.grau('C'), 7)
+        self.assertEqual(self.g_p.grau('E'), 2)
+        self.assertEqual(self.g_p.grau('P'), 2)
+        self.assertEqual(self.g_p.grau('M'), 2)
+        self.assertEqual(self.g_p.grau('T'), 3)
+        self.assertEqual(self.g_p.grau('Z'), 1)
+        with self.assertRaises(VerticeInvalidoException):
+            self.assertEqual(self.g_p.grau('G'), 5)
+
+        self.assertEqual(self.g_d.grau('A'), 1)
+        self.assertEqual(self.g_d.grau('C'), 0)
+        self.assertNotEqual(self.g_d.grau('D'), 2)
+
+        # Completos
+        self.assertEqual(self.g_c.grau('J'), 3)
+        self.assertEqual(self.g_c.grau('C'), 3)
+        self.assertEqual(self.g_c.grau('E'), 3)
+        self.assertEqual(self.g_c.grau('P'), 3)
+
+        # Com laço. Lembrando que cada laço conta uma única vez por vértice para cálculo do grau
+        self.assertEqual(self.g_l1.grau('A'), 3)
+        self.assertEqual(self.g_l2.grau('B'), 3)
+        self.assertEqual(self.g_l4.grau('D'), 1)
+
+    def test_ha_paralelas(self):
+        self.assertTrue(self.g_p.ha_paralelas())
+        self.assertFalse(self.g_p_sem_paralelas.ha_paralelas())
+        self.assertFalse(self.g_c.ha_paralelas())
+        self.assertFalse(self.g_c2.ha_paralelas())
+        self.assertFalse(self.g_c3.ha_paralelas())
+        self.assertTrue(self.g_l1.ha_paralelas())
+
+    def test_arestas_sobre_vertice(self):
+        self.assertEqual(set(self.g_p.arestas_sobre_vertice('J')), set(['a1']))
+        self.assertEqual(set(self.g_p.arestas_sobre_vertice('C')), set(['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7']))
+        self.assertEqual(set(self.g_p.arestas_sobre_vertice('M')), set(['a7', 'a8']))
+        self.assertEqual(set(self.g_l2.arestas_sobre_vertice('B')), set(['a1', 'a2', 'a3']))
+        self.assertEqual(set(self.g_d.arestas_sobre_vertice('C')), set())
+        self.assertEqual(set(self.g_d.arestas_sobre_vertice('A')), set(['asd']))
+        with self.assertRaises(VerticeInvalidoException):
+            self.g_p.arestas_sobre_vertice('A')
+
+    def test_eh_completo(self):
+        self.assertFalse(self.g_p.eh_completo())
+        self.assertFalse((self.g_p_sem_paralelas.eh_completo()))
+        self.assertTrue((self.g_c.eh_completo()))
+        self.assertTrue((self.g_c2.eh_completo()))
+        self.assertTrue((self.g_c3.eh_completo()))
+        self.assertFalse((self.g_l1.eh_completo()))
+        self.assertFalse((self.g_l2.eh_completo()))
+        self.assertFalse((self.g_l3.eh_completo()))
+        self.assertFalse((self.g_l4.eh_completo()))
+        self.assertFalse((self.g_l5.eh_completo()))
---
+++ meu_grafo.py
@@ -0,0 +1,54 @@
+from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
+from bibgrafo.grafo_exceptions import *
+
+
+class MeuGrafo(GrafoListaAdjacencia):
+
+    def vertices_nao_adjacentes(self):
+        '''
+        Provê uma lista de vértices não adjacentes no grafo. A lista terá o seguinte formato: [X-Z, X-W, ...]
+        Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
+        :return: Uma lista com os pares de vértices não adjacentes
+        '''
+        pass # Apague essa instrução e inicie seu código aqui
+
+    def ha_laco(self):
+        '''
+        Verifica se existe algum laço no grafo.
+        :return: Um valor booleano que indica se existe algum laço.
+        '''
+        pass
+
+    def grau(self, V=''):
+        '''
+        Provê o grau do vértice passado como parâmetro
+        :param V: O rótulo do vértice a ser analisado
+        :return: Um valor inteiro que indica o grau do vértice
+        :raises: VerticeInvalidoException se o vértice não existe no grafo
+        '''
+        pass
+
+    def ha_paralelas(self):
+        '''
+        Verifica se há arestas paralelas no grafo
+        :return: Um valor booleano que indica se existem arestas paralelas no grafo.
+        '''
+        pass
+
+    def arestas_sobre_vertice(self, V):
+        '''
+        Provê uma lista que contém os rótulos das arestas que incidem sobre o vértice passado como parâmetro
+        :param V: O vértice a ser analisado
+        :return: Uma lista os rótulos das arestas que incidem sobre o vértice
+        :raises: VerticeInvalidoException se o vértice não existe no grafo
+        '''
+        pass
+
+    def eh_completo(self):
+        '''
+        Verifica se o grafo é completo.
+        :return: Um valor booleano que indica se o grafo é completo
+        '''
+        pass
+
+

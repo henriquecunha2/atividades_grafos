21c495ea3a0b7177f54a2965db0357327b9ef3d3
Henrique Cunha <henrique.cunha@ifpb.edu.br>
2022-10-06 13:06:42 -0300

    Pequena correção de inclusão de import em grafo_lista_adj_test.py

--- grafo_lista_adj_test.py
+++ grafo_lista_adj_test.py
@@ -1,6 +1,7 @@
 import unittest
 from meu_grafo_lista_adj import *
 from bibgrafo.grafo_errors import *
+from bibgrafo.aresta import Aresta, ArestaDirecionada
 
 
 class TestGrafo(unittest.TestCase):
